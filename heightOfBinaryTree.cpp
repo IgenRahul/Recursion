da57fb231b290f0a41d8a6778afe212cf8c3afcc
Rahul Negi <rahul.negi.9078@gmail.com>
2022-03-04 15:11:38 +0530

    ...

---
+++ heightOfBinaryTree.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+//suppose a tree is given as head
+class tree{
+    public:
+    int data;
+    tree * left;
+    tree * right;
+    tree(){
+        data = 0;
+        left = NULL;
+        right = NULL;
+    }
+    tree(int a){
+        data = a;
+        left = NULL;
+        right = NULL;
+    }
+}
+int solve(tree * head){
+    if(head == NULL) return 0;
+    return 1+ max(solve(head->left),solve(head->right));
+}
+int main (){
+    tree * head(2);
+    cout<<solve(head);
+    return 0;
+}
